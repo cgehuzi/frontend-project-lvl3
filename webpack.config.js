3909671a3a04702df3498d42eeb1b2ac3ef12f5c
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-04-22 15:19:20 +0200

    fix: fix babel config for good work import on test's files

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,7 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.(c|sc|sa)ss$/i,
